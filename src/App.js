9827923a0f5b2c648f7bc0e440c24d4eb07cf4c5
kiloa44 <khaledalmansor888@gmail.com>
2019-12-28 13:17:05 +0200

    routers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignUp from './components/signup';
+import Home from './components/home';
+import Login from './components/login';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <SignUp></SignUp>
+      <BrowserRouter>
+       <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+          <li>
+            <Link to="/login">Dashboard</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <SignUp></SignUp>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
