292b2c6bb861a4e51ace6b59931dfe35bd43c5e4
kiloa44 <khaledalmansor888@gmail.com>
2019-12-28 13:10:34 +0200

    defaults again

---
+++ src/components/login.js
@@ -0,0 +1,8 @@
+import react from 'react';
+export default function Login() {
+    return (
+      <div>
+        <h2>Login</h2>
+      </div>
+    );
+  }
---
+++ src/components/signup.js
@@ -0,0 +1,8 @@
+import react from 'react';
+export default function SignUp() {
+    return (
+      <div>
+        <h2>SignUp</h2>
+      </div>
+    );
+  }
